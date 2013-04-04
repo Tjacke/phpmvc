e75407eeb194d8540c320e7155bffb0446e3ea66
Hakan Aslin <tjacke@hotmail.com>
2013-04-04 14:51:53 +0200

    adding new files to init project

---
+++ hello.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Min Sida</title>
+</head>
+<body>
+<h1>Hello World</h1>
+<?php echo "Min sida är i fas ett"; ?>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,4 @@
+This is the readme file.
+========================
+
+Describe project here so all can download it.
